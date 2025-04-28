6127efc162a81a595ef9f3c85b3c4a3173d3668c
Cici Woolf <cwoolf@mac.mynetworksettings.com>
2025-04-28 17:18:54 -0500

    Overlay with title on paintings

--- artwebsite/components/ArtGrid.js
+++ artwebsite/components/ArtGrid.js
@@ -2,16 +2,21 @@ import styles from './art-grid.module.css';
 import Image from 'next/image';
 
 export const ArtGrid = ({data}) => {
-  console.log('ArtGrid data:', data);
   return (
     <div className={styles.gridContainer}>
         <div className={styles.grid}>
             {data.map((item, index) => (
-                <div key={index} className={styles.gridItem}>
-                    <Image width="400" height="400" src={item.mediaUrl} alt={item.paintingTitle} />
-                </div>
+              <div key={index} className={styles.gridItem}>
+                  <Image width="400" height="400" src={item.mediaUrl} alt={item.paintingTitle} />
+                  <div className={styles.gridOverlay}>
+                    <div className={styles.overlayContent}>
+                      <p className={styles.overlayTitle}>{item.paintingTitle}</p>
+                      {/* <p className={styles.overlayArtist}>{item.description}</p> */}
+                    </div>
+                  </div>  
+              </div>
             ))}
-        </div>
+        </div> 
     </div>
   )
 };
