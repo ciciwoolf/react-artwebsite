8648701ee119a70af35e46d0b47391eee6c0455a
Cici Woolf <cwoolf@mac.mynetworksettings.com>
2025-05-02 11:55:21 -0500

    Adjust overlay to appear on active state in mobile

--- artwebsite/components/ArtGrid.js
+++ artwebsite/components/ArtGrid.js
@@ -1,7 +1,30 @@
+'use client';
+import { useEffect } from 'react';
 import styles from './art-grid.module.css';
 import Image from 'next/image';
 
 export const ArtGrid = ({data}) => {
+  useEffect(() => {
+    const gridItems = document.querySelectorAll(`.${styles.gridItem}`);
+
+    const handleClick = (event) => {
+      const overlay = event.currentTarget.querySelector(`.${styles.gridOverlay}`);
+      if (overlay) {
+        overlay.classList.toggle('active');
+      }
+    };
+
+    gridItems.forEach((item) => {
+      item.addEventListener('click', handleClick);
+    });
+
+    return () => {
+      gridItems.forEach((item) => {
+        item.removeEventListener('click', handleClick)
+      })
+    }
+  }, []);
+
   return (
     <div className={styles.gridContainer}>
         <div className={styles.grid}>
